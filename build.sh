774e84f98b1f3db5bfb00708b945fd526997fd11
YuzakiKokuban <heibanbaize@gmail.com>
2025-02-16 00:00:30 +0800

    first commit

---
+++ build.sh
@@ -0,0 +1,49 @@
+export PATH="/home/kokuban/toolchain/prebuilts/clang/host/linux-x86/clang-r450784e/bin:usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin:$PATH"
+
+echo $PATH
+
+set -e
+
+LTO=thin
+
+TARGET_DEFCONFIG=${1:-kalama_gki_defconfig}
+
+cd "$(dirname "$0")"
+
+LOCALVERSION=-Kokuban-android13-8-Hutao
+
+ARGS="
+CC=clang
+ARCH=arm64
+LLVM=1 LLVM_IAS=1
+LOCALVERSION=$LOCALVERSION
+"
+
+# build kernel
+make -j$(nproc) -C $(pwd) O=$(pwd)/out ${ARGS} $TARGET_DEFCONFIG
+
+./scripts/config --file out/.config \
+  -d UH \
+  -d RKP \
+  -d KDP \
+  -d SECURITY_DEFEX \
+  -d INTEGRITY \
+  -d FIVE \
+  -d TRIM_UNUSED_KSYMS
+
+if [ "$LTO" = "thin" ]; then
+  ./scripts/config --file out/.config -e LTO_CLANG_THIN -d LTO_CLANG_FULL
+fi
+
+make -j$(nproc) -C $(pwd) O=$(pwd)/out ${ARGS}
+
+# pack AnyKernel3
+cd out
+if [ ! -d AnyKernel3 ]; then
+  git clone --depth=1 https://github.com/YuzakiKokuban/AnyKernel3.git -b kalama_tab
+fi
+cp arch/arm64/boot/Image AnyKernel3/zImage
+name=${TARGET_DEFCONFIG%%_defconfig}_kernel_`cat include/config/kernel.release`_`date '+%Y_%m_%d'`
+cd AnyKernel3
+zip -r ${name}.zip * -x *.zip
+echo "AnyKernel3 package output to $(realpath $name).zip"
